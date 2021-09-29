43c58069dd54c2987306b4005b490a64ea6fb799
Bataus Alin <alin.bataus@stud.ubbcluj.ro>
2021-09-29 19:23:28 +0300

    am terminat

--- main.py
+++ main.py
@@ -1,33 +1,75 @@
+import math
+
 '''
 Returneaza true daca n este prim si false daca nu.
 '''
+
+
 def is_prime(n):
-  # codul vostru aici
-  
-  
+    if n < 2:
+        return False
+
+    if n == 2 or n == 3:
+        return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    for i in range(3, int(math.sqrt(n)), 2):
+        if n % i == 0:
+            return False
+
+    return True
+    pass
+
+
 '''
 Returneaza produsul numerelor din lista lst.
 '''
+
+
 def get_product(lst):
-  # codul vostru aici
-  
-  
+    s = 1
+    for _ in lst:
+        s *= _
+    return s
+    pass
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
+
+
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
+    while x != y:
+        if x > y:
+            y -= x
+        else:
+            x -= y
+
+    return x
+    pass
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
+
+
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
-  
-  
+    while y != 0:
+        rest = x % y
+        x = y
+        y = rest
+    return x
+
+    pass
+
+
 def main():
-  # interfata de tip consola aici
+    pass
+
 
 if __name__ == '__main__':
-  main()
+    main()
